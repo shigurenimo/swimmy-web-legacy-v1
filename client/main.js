ad6901e75f9e875035582b8c253680f3d409e5f6
takata <takata@sankaku.io>
2017-05-25 04:01:52 +0900

    modify : スマートフォンの操作性の改善

--- client/main.js
+++ client/main.js
@@ -2,13 +2,10 @@ import { Provider } from 'mobx-react'
 import React from 'react'
 import { render } from 'react-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import injectTapEventPlugin from 'react-tap-event-plugin'
 import { App } from './containers/app'
 import stores from './stores'
 import './router'
 import './styles/main'
-
-injectTapEventPlugin()
 
 render(
   <Provider {...stores}>
