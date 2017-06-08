18d1cc606838d283a38a74182b232ed1223a0f0a
takata <takata@sankaku.io>
2017-06-08 21:32:42 +0900

    modify : Report デザインの更新

--- client/containers/report.js
+++ client/containers/report.js
@@ -1,38 +1,51 @@
 import { inject, observer } from 'mobx-react'
 import React, { Component } from 'react'
 import propTypes from 'prop-types'
+import Typography from 'material-ui/Typography'
+import Layout from '../components/ui-layout'
+import Sheet from '../components/ui-sheet'
+import SheetContent from '../components/ui-sheet-content'
 
 @inject('reports')
 @observer
 export default class Report extends Component {
   render () {
-    return <div className='container:report'>
-      <div className='block:report-data'>
-        <div className='block:report'>
-          <div className='text:number'>{this.props.reports.index.total.users}</div>
-          <div className='text:description'>ユーザ数</div>
-        </div>
-        <div className='block:report'>
-          <div className='text:number'>{this.props.reports.index.total.posts}</div>
-          <div className='text:description'>書き込み</div>
-        </div>
-        <div className='block:report'>
-          <div className='text:number'>{this.props.reports.index.total.artworks}</div>
-          <div className='text:description'>アートワーク</div>
-        </div>
-      </div>
-      {this.props.reports.index.user &&
-      <div className='block:report-data'>
-        <div className='block:report'>
-          <div className='text:number'>{this.props.reports.index.user.posts}</div>
-          <div className='text:description'>あなたの書き込み</div>
-        </div>
-        <div className='block:report'>
-          <div className='text:number'>{this.props.reports.index.user.artworks}</div>
-          <div className='text:description'>あなたのアートワーク</div>
-        </div>
-      </div>}
-    </div>
+    return (
+      <Layout>
+        <Sheet>
+          <SheetContent>
+            <Typography type='display1'>{this.props.reports.index.total.users}</Typography>
+            <Typography>ユーザ数</Typography>
+          </SheetContent>
+        </Sheet>
+        <Sheet>
+          <SheetContent>
+            <Typography type='display1'>{this.props.reports.index.total.posts}</Typography>
+            <Typography>書き込み</Typography>
+          </SheetContent>
+        </Sheet>
+        <Sheet>
+          <SheetContent>
+            <Typography type='display1'>{this.props.reports.index.total.artworks}</Typography>
+            <Typography>アートワーク</Typography>
+          </SheetContent>
+        </Sheet>
+        {this.props.reports.index.user &&
+        <Sheet>
+          <SheetContent>
+            <Typography type='display1'>{this.props.reports.index.user.posts}</Typography>
+            <Typography>あなたの書き込み</Typography>
+          </SheetContent>
+        </Sheet>}
+        {this.props.reports.index.user &&
+        <Sheet>
+          <SheetContent>
+            <Typography type='display1'>{this.props.reports.index.user.artworks}</Typography>
+            <Typography>あなたのアートワーク</Typography>
+          </SheetContent>
+        </Sheet>}
+      </Layout>
+    )
   }
 
   static get contextTypes () {
