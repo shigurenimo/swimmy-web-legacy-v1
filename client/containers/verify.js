23a2e33ecc40c960d2d3be65d5b2d2b8a3426199
takata <takata@sankaku.io>
2017-06-12 03:02:25 +0900

    modify : Verify デザインの修正

--- client/containers/verify.js
+++ client/containers/verify.js
@@ -1,32 +1,39 @@
 import { inject, observer } from 'mobx-react'
 import React, { Component } from 'react'
+import Button from 'material-ui/Button'
+import Typography from 'material-ui/Typography'
+import Layout from '../components/ui-layout'
+import Sheet from '../components/ui-sheet'
+import SheetContent from '../components/ui-sheet-content'
 
 @inject('router')
 @observer
 export default class Verify extends Component {
   render () {
-    return <div className='container:verify'>
-      <div className='block:layout'>
-        <div className='block:verify-message'>
-          <div className='text:verify-message'>
-            {this.verifyMessage}
-          </div>
-        </div>
-        <div className='block:home'>
-          <input
-            className='text:home'
-            type='button'
-            value='アプリにもどる'
-            onTouchTap={this.onClose.bind(this)}/>
-        </div>
-      </div>
-    </div>
+    return (
+      <Layout>
+        <Sheet>
+          <SheetContent>
+            <Typography>
+              {this.verifyMessage}
+            </Typography>
+          </SheetContent>
+          <SheetContent>
+            <Button onClick={this.onClose}>
+              アプリにもどる
+            </Button>
+          </SheetContent>
+        </Sheet>
+      </Layout>
+    )
   }
 
   // ウィンドウを閉じる
   onClose () {
     window.open('about:blank', '_self').close()
   }
+
+  onClose = ::this.onClose
 
   get verifyMessage () {
     if (this.props.router.verifyError === null) {
