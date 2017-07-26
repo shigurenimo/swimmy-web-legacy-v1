3e2ef26bc615783d3d95219fb2ac1c4a8aca90db
uufish <uufish@sankaku.io>
2017-07-27 00:06:04 +0900

    fix : packages/routes

--- client/imports/packages/routes/model.js
+++ client/imports/packages/routes/model.js
@@ -2,10 +2,10 @@ import { types } from 'mobx-state-tree'
 import page from 'page'
 
 export default types.model('Routes', {
-  page: '',
-  template: '',
-  pageCache: '',
-  scrollCache: 0,
+  page: types.maybe(types.string),
+  template: types.maybe(types.string),
+  pageCache: types.maybe(types.string),
+  scrollCache: types.optional(types.number, 0),
   go (href) {
     page(href)
   },
