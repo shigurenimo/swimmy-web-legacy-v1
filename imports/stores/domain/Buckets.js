96388037c5381f7a7ee31446cdad41cbad4eef79
uufish <uufish@sankaku.io>
2017-10-19 02:35:23 +0900

    remove : stores/domain

--- imports/stores/domain/Buckets.js
+++
@@ -1,8 +0,0 @@
-import { types } from 'mobx-state-tree'
-import { createModel } from '/imports/packages/Sub'
-import Bucket from '/imports/models/Bucket'
-
-export default types
-.model('Buckets', {
-  // model: createModel(Bucket)
-})
