68e95a41156b35f013d682708b2cf7fb20b627e9
uufish <uufish@sankaku.io>
2017-10-22 04:30:59 +0900

    add : config

---
+++ imports/config/reservedWord.js
@@ -0,0 +1,18 @@
+export default [
+  "swimmy",
+  "default",
+  "self",
+  "follows",
+  "timemachine",
+  "thread",
+  "artwork",
+  "uuid",
+  "network",
+  "room",
+  "verify",
+  "verify-email",
+  "admin",
+  "config",
+  "release",
+  "report"
+]
