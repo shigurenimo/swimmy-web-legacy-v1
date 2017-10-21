2770d4cb17e3ef6f02aaa30406936a16aa920459
uufish <uufish@sankaku.io>
2017-10-22 04:55:07 +0900

    update : コンテンツの変更

--- imports/client/ui/pages/Admin/index.js
+++ imports/client/ui/pages/Admin/index.js
@@ -1,78 +1,49 @@
 import { Meteor } from 'meteor/meteor'
 
+import { inject } from 'mobx-react'
 import withStyles from 'material-ui/styles/withStyles'
 import Grid from 'material-ui/Grid'
-import TextField from 'material-ui/TextField'
 import Typography from 'material-ui/Typography'
 import compose from 'ramda/src/compose'
 import React, { Component } from 'react'
 
+import CodeIcon from '/imports/client/ui/components/CodeIcon'
 import Layout from '/imports/client/ui/components/Layout'
+import NowLoading from '/imports/client/ui/components/NowLoading'
 import withCurrentUser from '/imports/client/ui/hocs/withCurrentUser'
 import withScrollTop from '/imports/client/ui/hocs/withScrollTop'
+import withMethod from '/imports/client/ui/hocs/withMethod'
+
 import styles from './index.style'
 
 class Admin extends Component {
   render () {
     const {classes, currentUser} = this.props
-    console.log(this.props)
-    if (this.props.isLoggingIn) return <div>loading...</div>
+    if (this.props.isLoggingIn) { return <NowLoading /> }
     return (
       <Layout>
         <Grid container>
           <Grid item xs={12}>
-            <Typography type='display1'>Account</Typography>
+            <Typography type='display1'>アカウント</Typography>
           </Grid>
           <Grid item xs={12}>
-            <TextField
-              value={this.state.username}
-              label='ユーザネーム'
-              maxLength={10}
-              onChange={this.onInputUsername}
-              onBlur={this.onCheckUsername} />
+            <Typography>
+              この機能は調整中です。
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <CodeIcon code={currentUser.profile.code.split('')} />
           </Grid>
         </Grid>
       </Layout>
     )
   }
-
-  state = {
-    name: '',
-    nameError: '',
-    username: '',
-    usernameError: '',
-    inputNewEmail: '',
-    inputNewEmailError: '',
-    oldPassword: '',
-    newPassword: ''
-  }
-
-  onInputUsername = (event) => {
-    event.persist()
-    const value = event.target.value
-    if (value.length > 20) return
-    this.setState({username: value})
-  }
-
-  onCheckUsername = () => {
-    const username = this.state.username
-    this.props.checkExistUsername(username)
-    .then(res => {
-      if (res) {
-        this.setState({usernameError: 'そのユーザネームは既に存在します'})
-      } else {
-        this.setState({usernameError: ''})
-      }
-    })
-    .catch(err => this.props.snackbar.setError(err))
-  }
-
-  // ユーザネームの更新を送信する
-  onSubmitUsername = () => {}
 }
 
 export default compose(
   withStyles(styles),
+  inject('snackbar'),
   withCurrentUser,
+  withMethod('updateUserUsername'),
   withScrollTop
 )(Admin)
--- imports/client/ui/pages/Admin/index.style.js
+++ imports/client/ui/pages/Admin/index.style.js
@@ -1,17 +1,5 @@
 export default theme => {
   return {
-    squares: {
-      margin: '0 auto',
-      width: '140px',
-      height: '140px',
-      borderEadius: '2px'
-    },
-    square: {
-      display: 'inline-block',
-      verticalAlign: 'top',
-      width: '20%',
-      height: '20%'
-    },
     icon: {
       display: 'block',
       margin: '0 auto',
