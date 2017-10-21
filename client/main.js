cf2b1c883fe5daf66201dd742d5def8917b7ebd2
uufish <uufish@sankaku.io>
2017-10-22 00:20:09 +0900

    update : notFoundの追加

--- client/main.js
+++ client/main.js
@@ -19,6 +19,7 @@ import withProvider from '/imports/client/ui/hocs/withProvider'
 import withRouterHistory from '/imports/client/ui/hocs/withRouterHistory'
 import Admin from '/imports/client/ui/pages/Admin'
 import Note from '/imports/client/ui/pages/Note'
+import NotFound from '/imports/client/ui/pages/NotFound'
 import Login from '/imports/client/ui/pages/Login'
 import Thread from '/imports/client/ui/pages/Thread'
 import ThreadIndex from '/imports/client/ui/pages/ThreadIndex'
@@ -37,18 +38,20 @@ export const App = props =>
         </Router>
         <ContentPadding>
           <Router history={props.history}>
-            {props.isLogged ? <Switch>
+            {props.userId ? <Switch>
               <Route exact path='/' component={Timeline} />
               <Route exact path='/note' component={Note} />
               <Route exact path='/thread' component={ThreadIndex} />
               <Route exact path='/thread/:postId' component={Thread} />
               <Route exact path='/admin' component={Admin} />
+              <Route component={NotFound} />
             </Switch> : <Switch>
               <Route exact path='/' component={Timeline} />
               <Route exact path='/note' component={Note} />
               <Route exact path='/thread' component={ThreadIndex} />
               <Route exact path='/thread/:postId' component={Thread} />
               <Route exact path='/admin' component={Login} />
+              <Route component={NotFound} />
             </Switch>}
           </Router>
         </ContentPadding>
