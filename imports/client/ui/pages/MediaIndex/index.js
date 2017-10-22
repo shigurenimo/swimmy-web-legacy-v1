8c5ada16345e36df631f9aaa2467564ea59df0b6
uufish <uufish@sankaku.io>
2017-10-22 20:25:03 +0900

    add : ui/pages/MediaIndex

---
+++ imports/client/ui/pages/MediaIndex/index.js
@@ -0,0 +1,41 @@
+import { observer } from 'mobx-react'
+import compose from 'ramda/src/compose'
+import React, { Component } from 'react'
+
+import Layout from '/imports/client/ui/components/Layout'
+import NowLoading from '/imports/client/ui/components/NowLoading'
+import Sheet from '/imports/client/ui/components/Sheet'
+import SheetContent from '/imports/client/ui/components/SheetContent'
+import CardPost from '/imports/client/ui/containers/CardPost/index'
+import withMedia from '/imports/client/ui/hocs/withMedia'
+import withRouter from '/imports/client/ui/hocs/withRouter'
+import withScrollTop from '/imports/client/ui/hocs/withScrollTop'
+
+class Timeline extends Component {
+  render () {
+    return (
+      <Layout>
+        {this.forPosts}
+      </Layout>
+    )
+  }
+
+  get forPosts () {
+    if (this.props.posts.data.length === 0) {
+      return (
+        <Sheet>
+          <SheetContent>
+            {this.props.posts.loading && <NowLoading />}
+          </SheetContent>
+        </Sheet>
+      )
+    }
+    return this.props.posts.data.map(item => <CardPost key={item._id} {...item} />)
+  }
+}
+
+export default compose(
+  withRouter,
+  withMedia(),
+  withScrollTop,
+)(Timeline)
