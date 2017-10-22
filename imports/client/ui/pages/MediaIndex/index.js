5828f2865b2a06846aeddf41acefa6a765dbd251
uufish <uufish@sankaku.io>
2017-10-22 23:04:21 +0900

    update

--- imports/client/ui/pages/MediaIndex/index.js
+++ imports/client/ui/pages/MediaIndex/index.js
@@ -1,12 +1,9 @@
-import { observer } from 'mobx-react'
 import compose from 'ramda/src/compose'
 import React, { Component } from 'react'
 
 import Layout from '/imports/client/ui/components/Layout'
 import NowLoading from '/imports/client/ui/components/NowLoading'
-import Sheet from '/imports/client/ui/components/Sheet'
-import SheetContent from '/imports/client/ui/components/SheetContent'
-import CardImage from '/imports/client/ui/containers/CardImage'
+import CardImage from '/imports/client/ui/containers/CardImage/index'
 import withMedia from '/imports/client/ui/hocs/withMedia'
 import withRouter from '/imports/client/ui/hocs/withRouter'
 import withScrollTop from '/imports/client/ui/hocs/withScrollTop'
@@ -22,13 +19,7 @@ class Timeline extends Component {
 
   get forPosts () {
     if (this.props.posts.data.length === 0) {
-      return (
-        <Sheet>
-          <SheetContent>
-            {this.props.posts.loading && <NowLoading />}
-          </SheetContent>
-        </Sheet>
-      )
+      return this.props.posts.loading && <NowLoading />
     }
     return this.props.posts.data.map(item => <CardImage key={item._id} {...item} />)
   }
@@ -37,5 +28,5 @@ class Timeline extends Component {
 export default compose(
   withRouter,
   withMedia(),
-  withScrollTop,
+  withScrollTop
 )(Timeline)
