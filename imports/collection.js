2672f9d43b90780502d0a8e5202630a092dd25bf
uufish <uufish@sankaku.io>
2017-10-23 03:03:56 +0900

    refactor

--- imports/collection.js
+++ imports/collection.js
@@ -1,7 +1,6 @@
 import { Mongo } from 'meteor/mongo'
 
 export default {
-  artworks: new Mongo.Collection('artworks'),
   buckets: new Mongo.Collection('buckets'),
   channels: new Mongo.Collection('channels'),
   posts: new Mongo.Collection('posts'),
