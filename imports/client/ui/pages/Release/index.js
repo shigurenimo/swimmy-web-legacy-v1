37813141f698459c3ccb072f44ebe652a50edee7
uufish <uufish@sankaku.io>
2017-10-24 00:04:35 +0900

    add : ui/pages/Release

---
+++ imports/client/ui/pages/Release/index.js
@@ -0,0 +1,35 @@
+import withStyles from 'material-ui/styles/withStyles'
+import Typography from 'material-ui/Typography'
+import compose from 'ramda/src/compose'
+import map from 'ramda/src/map'
+import React from 'react'
+
+import Layout from '/imports/client/ui/components/Layout'
+import Sheet from '/imports/client/ui/components/Sheet'
+import SheetContent from '/imports/client/ui/components/SheetContent'
+import withScrollTop from '/imports/client/ui/hocs/withScrollTop'
+import releases from '/imports/config/release'
+
+import styles from './index.style'
+
+export const Release = props =>
+  <Layout>
+    {map(mapNote(props), releases)}
+  </Layout>
+
+export const mapNote = props => item =>
+  <Sheet hover key={item.version}>
+    <SheetContent>
+      <Typography className={props.classes.version} type='display1'>
+        {item.version}
+      </Typography>
+    </SheetContent>
+    <SheetContent>
+      <Typography dangerouslySetInnerHTML={{__html: item.content.join('</br>')}} />
+    </SheetContent>
+  </Sheet>
+
+export default compose(
+  withStyles(styles),
+  withScrollTop
+)(Release)
