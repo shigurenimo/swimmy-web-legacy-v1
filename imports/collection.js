aa054b7129e3c068d7f73e97730b45f7aa2df9b7
uufish <uufish@sankaku.io>
2017-11-02 10:12:50 +0900

    refactor

--- imports/collection.js
+++ imports/collection.js
@@ -3,6 +3,4 @@ import { Mongo } from 'meteor/mongo'
 export const Buckets = new Mongo.Collection('buckets')
 export const Channels = new Mongo.Collection('channels')
 export const Posts = new Mongo.Collection('posts')
-export const Reports = new Mongo.Collection('reports')
 export const Logs = new Mongo.Collection('logs')
-
